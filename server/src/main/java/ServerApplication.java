61763ac4ef0aa7ae58e53d1526342def46c5a8f1
Pavel Maslov <pavelm320@gmail.com>
2022-04-05 20:43:42 +0700

    first project commit

---
+++ server/src/main/java/ServerApplication.java
@@ -0,0 +1,8 @@
+import org.springframework.boot.SpringApplication;
+
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
